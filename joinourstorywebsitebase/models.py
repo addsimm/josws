9dbebcb65b92db79751fc7ac6bcee10d93a3273f
adamsimon <asimon@frameworksinstitute.org>
2015-02-28 12:26:22 -0800

    Started on Contact Form
    2Do:
      login
      recruit submit form
      svg

--- joinourstorywebsitebase/models.py
+++ joinourstorywebsitebase/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+
+from google.appengine.ext import ndb
 
 # Create your models here.
+
+def JOSContactMessageSSS_key():
+    """Constructs a Datastore key for the JOSContactMessage entity named JOSContactMessagesss."""
+    return ndb.Key('JOSContactMessageSSS', 'JOSContactMessageSSS')
+
+class JOSContactMessage(ndb.Model):
+    jcm_sender = ndb.UserProperty()
+    jcm_first_name = ndb.StringProperty()
+    jcm_last_name = ndb.StringProperty()
+    jcm_email_address = ndb.StringProperty()
+    jcm_message = ndb.TextProperty()
+    jcm_date_created = ndb.DateTimeProperty(auto_now_add = True)
+
--- joinourstorywebsitebase/views.py
+++ joinourstorywebsitebase/views.py
@@ -1,8 +1,42 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
+from google.appengine.api import users
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response, redirect
+from joinourstorywebsitebase.models import *
 
 def index(request):
    template_values = {}
 
    return render_to_response('index.html', template_values)
+
+
+def contactus(request):
+    template_values = {}
+    if request.method == 'POST':
+        joscontactmessage = JOSContactMessage(parent = JOSContactMessageSSS_key())  # finicky
+
+        if users.get_current_user():
+            joscontactmessage.jcm_sender = users.get_current_user()
+        else:
+            joscontactmessage.jcm_sender = "unknown"
+
+
+        # implement
+        joscontactmessage.dd_url = request.POST.get('url')
+
+        joscontactmessage.put()
+        return # ??
+
+    page_name = 'Contact Us'
+
+
+
+    # template_values = return_template_values(path) #implement
+    # template_values.update(csrf(request))
+    # template_values.update({'page_name': page_name,
+    #                         'discussedDocs': discussed_docs,
+    #                         'currentDocs': current_docs
+    # })
+
+    return render_to_response('contactus.html', template_values)
