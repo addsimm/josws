7cc7cbaf9ddaf5bd177172b001098928469340f6
addsimm <adamfsimon@gmail.com>
2015-04-12 11:39:47 -0700

    Did contact us submit; contact us email roughed out.

--- joinourstorywebsitebase/views.py
+++ joinourstorywebsitebase/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 from __future__ import print_function
-from google.appengine.api import users
+
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, redirect
 from joinourstorywebsitebase.models import *
+
+from google.appengine.api import users
+from google.appengine.api import mail
 
 
 def index(request):
@@ -28,14 +31,28 @@ def contactus(request):
 
         joscontactmessage.jcm_first_name = request.POST['firstnamefield']
         joscontactmessage.jcm_last_name = request.POST['lastnamefield']
-        joscontactmessage.jcm_email_address = request.POST['emailfield']
+
         joscontactmessage.jcm_message = request.POST['messagefield']
 
-        print("joscontactmessage:", joscontactmessage.jcm_message)
+        user_email_address = request.POST['emailfield']
+        print("joscontactemail:", joscontactmessage.jcm_message)
 
+
+        if mail.is_email_valid(user_email_address):
+            joscontactmessage.jcnm_email_address = user_email_address
+
+            sender_address = "Example.com Support <support@example.com>"  #change  VVVVVVVVVVV
+            subject = "Confirm your registration"
+            body = """
+                        Thank you for creating an account! Please confirm your email address by
+                        clicking on the link below:
+                        """  # %s then, confirmation_url
+
+            mail.send_mail(sender_address, user_email_address, subject, body)
+
+        else:
+            print(" bad email ")   # bad email return
 
         #joscontactmessage.put()
-
-    # template_values = return_template_values(path) #implement
 
     return render_to_response('contactus.html', template_values)
