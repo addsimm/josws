42096bc3c0de647b6a2a9b451e0006d31a213e5d
addsimm <adamfsimon@gmail.com>
2015-04-20 09:03:48 -0700

    Contact us response finished; upgrade validation to mailgun.

--- joinourstorywebsitebase/views.py
+++ joinourstorywebsitebase/views.py
@@ -3,10 +3,12 @@ from __future__ import print_function
 
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, redirect
-from joinourstorywebsitebase.models import *
+from django.core.validators import validate_email
 
 from google.appengine.api import users
 from google.appengine.api import mail
+
+from joinourstorywebsitebase.models import *
 
 
 def index(request):
@@ -20,11 +22,14 @@ def index(request):
 def contactus(request):
     template_values = {}
     template_values.update(csrf(request))
-    template_values.update({'page_name': 'Contact Us'
+    template_values.update({'page_name': 'Contact Us',
+                            'return_message_flag': 0,
+                            'return_message': ' '
                             })
+
     if request.method == 'POST':
         ### FOR DEBUG raise KeyError(request.POST)
-        joscontactmessage = JOSContactMessage(parent = JOSContactMessageSSS_key())
+        joscontactmessage = JOSContactMessage(parent = JOSContactMessageSSS_key())  # Message instance
 
         if users.get_current_user():
             joscontactmessage.jcm_sender = users.get_current_user()
@@ -35,24 +40,32 @@ def contactus(request):
         joscontactmessage.jcm_message = request.POST['messagefield']
 
         user_email_address = request.POST['emailfield']
-        print("joscontactemail:", joscontactmessage.jcm_message)
+        print("joscontactemail:", user_email_address)          ### Logging
 
-
-        if mail.is_email_valid(user_email_address):
+        try:                                                   # Confimation and email message to send
+            validate_email(user_email_address)
             joscontactmessage.jcnm_email_address = user_email_address
 
-            sender_address = "Example.com Support <support@example.com>"  #change  VVVVVVVVVVV
-            subject = "Confirm your registration"
-            body = """
-                        Thank you for creating an account! Please confirm your email address by
-                        clicking on the link below:
-                        """  # %s then, confirmation_url
+            sender_address = "Join Our Story <info@joinourstory.com>"
+            subject = "Your Recent Message"
+            body = "Hi {0}, We received your message! We will do our best to respond quickly - hopefully within 24 hours. Thank you, Adam --- Message: {1}".format(
 
-            mail.send_mail(sender_address, user_email_address, subject, body)
+                joscontactmessage.jcm_first_name, joscontactmessage.jcm_message)
 
-        else:
-            print(" bad email ")   # bad email return
+            mail.send_mail(sender_address, user_email_address, subject, body)           ### Reformat Body
+            mail.send_mail("adam@joinourstory.com", user_email_address, subject, body)  ### Reformat Body
+            template_values.update({'return_address': user_email_address,
+                                    'return_message_flag': 1,
+                                    'return_message': 'Email sent! If a confirmation email does not arrive soon, please try again or find help.'
+                                    })
+            joscontactmessage.put()
+        except:                                                                         # Validation Fail
+            template_values.update({'return_address': user_email_address,
+                                    'return_message_flag': 1,
+                                    'return_message': 'Email failed to send! Please try again or find help.'
+                                    })
 
-        #joscontactmessage.put()
+        return render_to_response('contactmessageresponse.html', template_values)
+
 
     return render_to_response('contactus.html', template_values)
