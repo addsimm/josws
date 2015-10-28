dc131f8fd8718b700f158a6dbdc6b12561dfff29
addsimm <adamfsimon@gmail.com>
2015-10-27 18:22:22 -0700

    working on landing page - now: finish button
     next: home page
    
     also need to add email storage and lookup.

--- joinourstorywebsitebase/models.py
+++ joinourstorywebsitebase/models.py
@@ -3,15 +3,13 @@ from google.appengine.ext import ndb
 
 # Create your models here.
 
-def JOSContactMessageSSS_key():
+def JOSUserSSS_key():
     """Constructs a Datastore key for the JOSContactMessage entity named JOSContactMessagesss."""
     return ndb.Key('JOSContactMessageSSS', 'JOSContactMessageSSS')
 
-class JOSContactMessage(ndb.Model):
-    jcm_sender = ndb.UserProperty()
-    jcm_first_name = ndb.StringProperty()
-    jcm_last_name = ndb.StringProperty()
-    jcm_email_address = ndb.StringProperty()
-    jcm_message = ndb.TextProperty()
-    jcm_date_created = ndb.DateTimeProperty(auto_now_add = True)
+class JOSUser(ndb.Model):
+    ju_first_name = ndb.StringProperty()
+    ju_last_name = ndb.StringProperty()
+    ju_messages = ndb.TextProperty(repeated=True)
+    ju_date_created = ndb.DateTimeProperty(auto_now_add = True)
 
--- joinourstorywebsitebase/views.py
+++ joinourstorywebsitebase/views.py
@@ -13,80 +13,90 @@ from google.appengine.api import mail   # --enable_
 
 from joinourstorywebsitebase.models import *
 
+import six
+
 
 def landingpage(request):
    template_values = {}
-   current_user = ''  ##### find current user
    context = RequestContext(request, template_values)
-   context.update({'page_name': 'Landing Page',
-                   'user': current_user
+   context.update({'page_name': 'Landing Page'
                    })
 
    return render_to_response('landingpage.html', context)
 
 def index(request):
    template_values = {}
-   current_user = ''  ##### find current user
    context = RequestContext(request, template_values)
-   context.update({'page_name': 'Home',
-                   'user': current_user
+   context.update({'page_name': 'Home'
                    })
+
+   if request.method == 'POST':
+        user_email_address = request.POST['emailfield']
+        print("JOS User Email Address:", user_email_address)    ### Logging
+
+        josuser = JOSUser.get_or_insert(key_name=user_email_address, parent=JOSUserSSS_key())
+
+        sender_address = "Adam <adam@joinourstory.com>" # must be google apps admin
+        subject = "Succesfully Subcribed to Hollywood Heights Updates"
+        body = "Thank you for supporting Hollywood Heights; please stay tuned for news and updates. If you do not receive our emails, check your spam folder and/or add adam@joinourstory.com to your contact list."
+
+        print("Contact Message Body:", body)
+        mail.send_mail(sender_address, user_email_address, subject, body)
+        mail.send_mail(sender_address, "info@joinourstory.com", subject + "; " + user_email_address, body)
+        context.update({'return_address': user_email_address,
+                        'return_message_flag': 1,
+                        'return_message': 'Subscription succssful! If a confirmation does not arrive soon, please check the address you gave and try again.'})
+        print("Message Saving")
+        josuser.put()
+
+        return render_to_response('contactmessageresponse.html', context)
+
 
    return render_to_response('index.html', context)
 
 
 def contactus(request):
-    context = {}
+    template_values = {}
+    context = RequestContext(request, template_values)
     context.update(csrf(request))
     context.update({'page_name': 'Contact Us',
-                            'return_message_flag': 0,
-                            'return_message': ' '
-                            })
+                    'return_message_flag': 0,
+                    'return_message': ' '
+                    })
 
     if request.method == 'POST':
-        ### FOR DEBUG raise KeyError(request.POST)
-        joscontactmessage = JOSContactMessage(parent = JOSContactMessageSSS_key())  # Message instance
+        user_email_address = request.POST['emailfield']
+        print("JOS User Email Address:", user_email_address)    ### Logging
 
-        if users.get_current_user():
-            joscontactmessage.jcm_sender = users.get_current_user()
+        josuser = JOSUser.get_or_insert(key_name=user_email_address, parent=JOSUserSSS_key())
 
-        joscontactmessage.jcm_first_name = request.POST['firstnamefield']
-        joscontactmessage.jcm_last_name = request.POST['lastnamefield']
-        joscontactmessage.jcm_message = request.POST['messagefield']
+        josuser.ju_first_name = request.POST['firstnamefield']
+        if isinstance(request.POST['lastnamefield'], six.string_types):
+            josuser.ju_last_name = request.POST['lastnamefield']
+        else:
+            josuser.ju_last_name = ' '
+
+        if isinstance(request.POST['messagefield'], six.string_types):
+            this_message = request.POST['messagefield']
+            user_messages = josuser.ju_messages
+            user_messages.append(this_message)
+        else:
+            this_message = 'Message not retrieved.'
 
         sender_address = "Adam <adam@joinourstory.com>" # must be google apps admin
+        subject = "Your Recent Message to JOS"
+        body = "Hi {0}, We received your message! We will do our best to respond quickly - hopefully,  within 24 hours. Thank you! --- Message: {1}".format(josuser.ju_first_name, this_message)
 
-        user_email_address = request.POST['emailfield']
-        # print("JOS Contactee Email Address:", user_email_address)          ### Logging
-
-        try:                                                   # Confimation and email message to send
-            validate_email(user_email_address)
-            joscontactmessage.jcm_email_address = user_email_address
-
-
-            subject = "Your Recent Message to JOS"
-            body = "Hi {0}, We received your message! We will do our best to respond quickly - hopefully,  within 24 hours. Thank you! --- Message: {1}".format(
-                joscontactmessage.jcm_first_name, joscontactmessage.jcm_message)
-
-            # print("Contact Message Body:", body)
-            mail.send_mail(sender_address, user_email_address, subject, body)  ### Reformat Body
-            mail.send_mail(sender_address, "info@joinourstory.com", subject + " " + user_email_address, body)  ### Reformat Body
-            context.update({'return_address': user_email_address,
-                                    'return_message_flag': 1,
-                                    'return_message': 'Email sent! If a confirmation email does not arrive soon, please check the address you gave us and try again.'
-                                    })
-            print("Message Saving")
-            joscontactmessage.put()
-
-        except:  #  Fail to send.
-
-            context.update({'return_address': user_email_address,
-                                    'return_message_flag': 1,
-                                    'return_message': 'Email failed to send... Please try again or get help.'
-                                    })
+        print("Contact Message Body:", body)
+        mail.send_mail(sender_address, user_email_address, subject, body)
+        mail.send_mail(sender_address, "info@joinourstory.com", subject + "; " + user_email_address, body)
+        context.update({'return_address': user_email_address,
+                        'return_message_flag': 1,
+                        'return_message': 'Message sent. If a confirmation does not arrive soon, please check the address you gave and try again.'})
+        print("Message Saving")
+        josuser.put()
 
         return render_to_response('contactmessageresponse.html', context)
-
 
     return render_to_response('contactus.html', context)
 
